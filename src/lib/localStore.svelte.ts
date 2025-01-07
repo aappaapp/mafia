1705009c5d550c537b63278bbb02a9fb2880c164
aappaapp <adenpun2000@gmail.com>
2025-01-07 23:59:44 +0800

    Add death tracker

--- src/lib/localStore.svelte.ts
+++ src/lib/localStore.svelte.ts
@@ -1,5 +1,3 @@
-// place files you want to import through the `$lib` alias in this folder.
-
 import { browser } from '$app/environment';
 
 export class LocalStore<T> {
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,33 +1,34 @@
 <script lang="ts">
-	import { localStore } from '$lib/index.svelte';
+	import { localStore } from '$lib';
 	import { onMount } from 'svelte';
 
+	type DeepPartial<T> = {
+		[P in keyof T]?: DeepPartial<T[P]>;
+	};
 	type State = {
 		playerCount?: number;
 		playerData?: {
 			role?: 'werewolf' | 'king' | 'witch' | 'seer' | 'hunter' | 'knight' | 'villager';
+			dead: boolean;
 		}[];
 	};
-	let state = localStore<State>('state', {
+	let state = localStore<DeepPartial<State>>('state', {
 		playerCount: 12,
 		playerData: []
 	});
 
 	$effect(() => {
-		localStorage.setItem('state', JSON.stringify(state.value));
-	});
-
-	$effect(() => {
-		if (state.value.playerCount !== state.value.playerData?.length) {
-			state.value.playerData = new Array(state.value.playerCount).fill({ role: 'villager' });
+		if (state.value.playerCount && state.value.playerData) {
+			if (state.value.playerData.length < state.value.playerCount) {
+				for (let i = state.value.playerData.length; i < state.value.playerCount; i++) {
+					state.value.playerData.push({
+						role: 'villager',
+						dead: false
+					});
+				}
+			}
 		}
 	});
-
-	onMount(() => {
-		state.value = JSON.parse(localStorage.getItem('state') ?? '{}');
-		console.log(localStorage.getItem('state'), JSON.parse(localStorage.getItem('state') ?? '{}'));
-	});
-	$inspect(state.value);
 </script>
 
 <div class="flex flex-col gap-4">
@@ -38,26 +39,30 @@
 			<input
 				type="number"
 				id="players"
-				bind:value={state.value.playerCount}
+				bind:value={() => state.value.playerCount,
+				(v) => {
+					state.value.playerCount = v ?? 0;
+				}}
 				class="b--input--number"
 			/>
 		</div>
 		{#each new Array(state.value.playerCount) as _, i}
-			<div class="flex flex-col rounded-lg border-2 border-black p-2">
+			<div
+				class="flex flex-col rounded-lg border-2 border-black p-2 {state.value.playerData?.[i]?.dead
+					? 'bg-black text-white'
+					: ''}"
+			>
 				<span>玩家編號{i + 1}</span>
 				<div>
 					<label for="role-{i}">職業</label>
 					<select
 						id="role-{i}"
-						bind:value={() => {
-							return state.value.playerData?.[i]?.role ?? 'villager';
-						},
+						bind:value={() => state.value.playerData?.[i]?.role ?? 'villager',
 						(v) => {
-							state.value.playerData ??= new Array(state.value.playerCount).fill({
-								role: 'villager'
-							});
-							state.value.playerData[i].role = v;
+							if (typeof state.value.playerData?.[i]?.role !== 'undefined')
+								state.value.playerData[i].role = v;
 						}}
+						class="bg-transparent"
 					>
 						<option value="werewolf">狼人</option>
 						<option value="king">狼王</option>
@@ -67,6 +72,18 @@
 						<option value="knight">騎士</option>
 						<option value="villager">村民</option>
 					</select>
+				</div>
+				<div>
+					<label for="dead-{i}">已死亡?</label>
+					<input
+						type="checkbox"
+						id="dead-{i}"
+						bind:checked={() => state.value.playerData?.[i]?.dead ?? false,
+						(v) => {
+							if (typeof state.value.playerData?.[i]?.dead !== 'undefined')
+								state.value.playerData[i].dead = v;
+						}}
+					/>
 				</div>
 			</div>
 		{/each}
